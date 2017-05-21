304ae095230c48aff97beae2e04de59091256a88
VanHakobyan <van19962013@mail.ru>
2017-05-21 17:52:08 +0400

    add changes sorting orderby,thenby

--- LINQ to Entity/Sorting/Program.cs
+++ LINQ to Entity/Sorting/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using DataContextLinq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataContextLinq
+namespace Sorting
 {
     class Program
     {
@@ -12,7 +13,21 @@ namespace DataContextLinq
         {
             using (LinqModel db = new LinqModel())
             {
+                //first version 
+                var phone = db.Phones.OrderBy(p => p.Name);
+                foreach (var p in phone)
+                {
+                    Console.WriteLine(p.Name + " " + p.Company + " " + p.Price);
+                }
 
+                //other version in linq
+                var phone2 = from p in db.Phones
+                             orderby p.Name
+                             select p;
+                foreach (var p in phone2)
+                {
+                    Console.WriteLine(p.Name + " " + p.Company + " " + p.Price);
+                }
                 //Descending
                 var phoneDec = db.Phones.OrderByDescending(p => p.Name);
                 foreach (var p in phoneDec)
@@ -30,7 +45,11 @@ namespace DataContextLinq
                 }
 
 
+
+
+
             }
+
         }
     }
 }
