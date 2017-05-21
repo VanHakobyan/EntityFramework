9efde4ad93024422e0bcfb0087e5774e124f0008
VanHakobyan <van19962013@mail.ru>
2017-05-21 20:35:52 +0400

    last changes :one::nine:

--- LINQ to Entity/DataContextLinq/Program.cs
+++ LINQ to Entity/DataContextLinq/Program.cs
@@ -12,18 +12,6 @@ namespace DataContextLinq
         {
             using (LinqModel db = new LinqModel())
             {
-                //linq 
-                var phonesLinq = from phone in db.Phones
-                                 group phone by phone.Company.Name;
-                foreach (var p in phonesLinq)
-                {
-                    Console.WriteLine(p.Key + p.Count());
-                    foreach (var g in p)
-                    {
-                        Console.WriteLine(g.Id + " " + g.Name + " " + g.Price + " " + g.Company);
-                    }
-                }
-
             }
         }
     }
