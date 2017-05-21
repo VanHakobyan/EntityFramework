a72e3ca6f113125d529d92d369442337cc279afe
VanHakobyan <van19962013@mail.ru>
2017-05-21 19:04:34 +0400

    add group by statment :one:

--- LINQ to Entity/GroupBy/Program.cs
+++ LINQ to Entity/GroupBy/Program.cs
@@ -1,20 +1,34 @@
-﻿using System;
+﻿using DataContextLinq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataContextLinq
+namespace GroupBy
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             using (LinqModel db = new LinqModel())
             {
+                //extension method group by
+                var phones = db.Phones.GroupBy(p => p.Company.Name);
+                foreach (var p in phones)
+                {
+                    Console.WriteLine(p.Key + p.Count());
+                    foreach (var g in p)
+                    {
+                        Console.WriteLine(g.Id + " " + g.Name + " " + g.Price + " " + g.Company);
+                    }
+                }
+
+
                 //linq 
                 var phonesLinq = from phone in db.Phones
-                                 group phone by phone.Company.Name;
+                                  group phone by phone.Company.Name;
                 foreach (var p in phonesLinq)
                 {
                     Console.WriteLine(p.Key + p.Count());
