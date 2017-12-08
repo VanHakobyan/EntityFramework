d6a8d6c41f2d719b34df6299f3313a92bd374b7b
van hakobyan <van19962013@mail.ru>
2017-12-08 16:20:05 +0400

    add core codeFirst

---
+++ EFcore/EFcoreLib/ContextCore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFcoreLib
+{
+    public class ContextCore : DbContext
+    {
+        public DbSet<SimpleModel> SimpleModels { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=newDb;Trusted_Connection=True;");
+        }
+    }
+}
